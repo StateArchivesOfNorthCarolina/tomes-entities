0088a0da2f249836e433b82b99366eef35fcefd8
Nitin Arora <nitaro74@gmail.com>
2018-02-27 10:22:27 -0500

    Added setup and requirements.

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, setuptools
+
+def doc():
+    with open("docs/documentation.md") as d:
+        return d.read()
+		
+setup(
+    name="tomes_entities",
+    version="0.0.1",
+    packages=setuptools.find_packages(),
+    python_requires=">=3",
+    url="https://github.com/StateArchivesOfNorthCarolina/tomes_entities",
+    license="LICENSE.txt",
+    author="Nitin Arora",
+    author_email="nitin.a.arora@ncdcr.gov",
+    description="Part of the TOMES project: creates a Stanford CoreNLP compliant RegexNER mappings file.",
+    long_description=doc(),
+)
+
