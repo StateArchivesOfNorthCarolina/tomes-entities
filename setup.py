a9540d571080e6367cb61615581c4c4e1ca5789c
Nitaro <nitaro74@gmail.com>
2018-06-05 16:49:09 -0400

    Updated imports.

--- setup.py
+++ setup.py
@@ -1,17 +1,18 @@
-import tomes_entities.entities as entities
 from setuptools import setup, setuptools
+from tomes_entities.entities import __NAME__, __FULLNAME__, __DESCRIPTION__, __URL__, 
+__VERSION__, __AUTHOR__, __AUTHOR_EMAIL_
 
 def doc():
     with open("docs/documentation.md") as d:
         return d.read()
 		
 setup(
-    name = entities.__NAME__,
-    description = entities.__DESCRIPTION__,
-    url = entities.__URL__,
-    version = entities.__VERSION__,
-    author = entities.__AUTHOR__,
-    author_email = entities.__AUTHOR_EMAIL__,
+    name = __NAME__,
+    description = __DESCRIPTION__,
+    url = __URL__,
+    version = __VERSION__,
+    author = __AUTHOR__,
+    author_email = __AUTHOR_EMAIL__,
     packages = setuptools.find_packages(),
     python_requires = ">=3",
     license = "LICENSE.txt",
